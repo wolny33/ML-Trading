1fb48e300e6ef44eb1a7e92dd0b70b31219a90dd
wolny33 <patrykburzycki@op.pl>
2023-10-30 22:43:13 +0100

    added Bot class

---
+++ TradingBot/Bot.cs
@@ -0,0 +1,22 @@
+﻿namespace TradingBot
+{
+    public class Bot
+    {
+        private float PredictPrice()
+        {
+            throw new NotImplementedException();
+        }
+        private void GetNextMoveFromStrategy()
+        {
+            throw new NotImplementedException();
+        }
+        private bool PlaceBuyOrder()
+        {
+            throw new NotImplementedException();
+        }
+        private bool PlaceSellOrder()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
