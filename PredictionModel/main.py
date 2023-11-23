607f296a97db2e248b6225aeecaf827d944867e9
Meeso1 <kubaknyspel@gmail.com>
2023-11-24 00:15:07 +0100

    feat: Better DTO structure + HTTP file.

--- PredictionModel/main.py
+++ PredictionModel/main.py
@@ -2,25 +2,60 @@ from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from keras.models import load_model
 import numpy as np
+from datetime import datetime
 
 app = FastAPI()
 model = load_model('model.h5')
 
 
+class DailyData(BaseModel):
+    open: float
+    close: float
+    high: float
+    low: float
+    volume: float
+    date: str
+
+
 class PredictRequest(BaseModel):
-    data: list[float]
+    data: list[DailyData]
 
 
 class PredictResponse(BaseModel):
-    output: list[float]
+    close: list[float]
+    high: list[float]
+    low: list[float]
 
 
 @app.post("/predict", response_model=PredictResponse)
 def predict(request: PredictRequest) -> PredictResponse:
     try:
-        input_data = np.array(np.reshape(request.data, (1, 10, 8)))
-        prediction = model.predict(input_data)
+        input_data = np.vstack([get_features_vector(v) for v in request.data])
 
-        return PredictResponse(output=[v for v in prediction.flatten()])
+        # TODO: scale...
+
+        reshaped = np.reshape(input_data, (1, 10, 8))
+        prediction = np.reshape(model.predict(reshaped), (-1, 3))
+
+        # TODO: descale...
+
+        close = [v for v in prediction[:, 0]]
+        high = [v for v in prediction[:, 1]]
+        low = [v for v in prediction[:, 2]]
+        return PredictResponse(close=close, high=high, low=low)
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
+
+
+def get_features_vector(data: DailyData) -> np.ndarray:
+    date = datetime.fromisoformat(data.date)
+    return np.array([
+        data.open,
+        data.close,
+        data.high,
+        data.low,
+        data.volume,
+        date.weekday() / 6,
+        np.sin(2 * np.pi * date.timetuple().tm_yday / 366),
+        np.cos(2 * np.pi * date.timetuple().tm_yday / 366)
+    ])
