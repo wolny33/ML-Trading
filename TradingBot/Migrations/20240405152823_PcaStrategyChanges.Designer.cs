bec9dc1a6cecbcbd4924aa993cfd2105fb58db0d
Meeso1 <39009805+Meeso1@users.noreply.github.com>
2024-04-05 17:29:03 +0200

    chore: Migration

--- TradingBot/Migrations/20240405152823_PcaStrategyChanges.Designer.cs
+++ TradingBot/Migrations/20240405152823_PcaStrategyChanges.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TradingBot.Database;
 
@@ -10,9 +11,11 @@ using TradingBot.Database;
 namespace TradingBot.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240405152823_PcaStrategyChanges")]
+    partial class PcaStrategyChanges
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "7.0.13");
